9fdb5bbb7f3ff71fa9e11df47450ec8bde92aee0
TrveBlacker <80486225+TrveBlacker@users.noreply.github.com>
2021-03-22 23:10:15 +0300

    Dash no cooldown

--- Map Gen/Assets/Scripts/Character/Blyat.cs
+++ Map Gen/Assets/Scripts/Character/Blyat.cs
@@ -4,27 +4,44 @@ using UnityEngine;
 
 public class Blyat : MonoBehaviour
 {
+    private Transform cursor;
+    public Transform cam;
+    [SerializeField] private LayerMask groundLayerMask;
+
+
     private Rigidbody rb;
 
     private bool isMoving;
     public float horizontal;
     public float vertical;
     public float timeToMove;
+    public float timeToDash;
+    public float turnTime;
+    public float turnVelocity;
     public float elapsedTime;
+
     public Vector3 newDirection;
     public Vector3 setDirection;
     public Vector3 startPosition;
     public Vector3 targetPosition;
-    
+
+    public float turnSpeed;
+
 
     void Start()
     {
-    	rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        cursor = GameObject.Find("CursorTarget").GetComponent<Transform>();
     }
 
     void Update()
     {
-        /* не робит без IsGrounded
+
+        float targetAngle = Mathf.Atan2((cursor.position - transform.position).x, (cursor.position - transform.position).z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        /* Nahuy
         if (!IsGrounded())
             rb.MovePosition(transform.position + Vector3.down * Time.deltaTime);
         */
@@ -32,12 +49,13 @@ public class Blyat : MonoBehaviour
         vertical = Input.GetAxisRaw("Vertical");
         newDirection = Quaternion.Euler(0, 45, 0) * new Vector3(horizontal, 0f, vertical);
         newDirection = new Vector3(Mathf.Round(newDirection.x), 0f, Mathf.Round(newDirection.z));
+
+        if (Input.GetKey(KeyCode.LeftShift) && newDirection.magnitude > 0 && !isMoving)
+            StartCoroutine(DashPlayer(newDirection));
+
         if (newDirection.magnitude > 0 && !isMoving)
-        {
-            setDirection = newDirection;
-            StartCoroutine(MovePlayer(setDirection));
-        }
-     }
+            StartCoroutine(MovePlayer(newDirection));
+    }
 
     private IEnumerator MovePlayer(Vector3 direction)
     {
@@ -48,25 +66,44 @@ public class Blyat : MonoBehaviour
         startPosition = new Vector3(Mathf.Round(transform.position.x), transform.position.y, Mathf.Round(transform.position.z));
         targetPosition = startPosition + direction;
 
-        while(elapsedTime < timeToMove)
+        while (elapsedTime < timeToMove)
         {
-            //transform.position = Vector3.Lerp(startPosition, targetPosition, (elapsedTime / timeToMove));
             rb.MovePosition(Vector3.Lerp(startPosition, targetPosition, (elapsedTime / timeToMove)));
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
-        //transform.position = targetPosition;
         rb.MovePosition(targetPosition);
 
         isMoving = false;
     }
-    /* сюда надо вписать слой
+
+    private IEnumerator DashPlayer(Vector3 direction)
+    {
+        isMoving = true;
+
+        float elapsedTime = 0;
+
+        startPosition = new Vector3(Mathf.Round(transform.position.x), transform.position.y, Mathf.Round(transform.position.z));
+        targetPosition = startPosition + direction * 3;
+
+        while (elapsedTime < timeToDash)
+        {
+            rb.MovePosition(Vector3.Lerp(startPosition, targetPosition, (elapsedTime / timeToMove)));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        rb.MovePosition(targetPosition);
+
+        isMoving = false;
+    }
+
+    /* E eto nahuy
     private bool IsGrounded()
     {
-        RaycastHit raycastHit = Physics.BoxCast(transform.position, new Vector3(0.5f, 0.5f, 0.5f), Vector3.down, Quaternion.identity, 1f);
-
-        return raycastHit.collider != null;
+        bool raycastHit = Physics.BoxCast(transform.position, new Vector3(0.5f, 0.5f, 0.5f), Vector3.down, transform.rotation, 1f, groundLayerMask);
+        return raycastHit;
     }
     */
 }
